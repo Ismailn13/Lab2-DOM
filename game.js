896eae6c841e3c36b4650fce2b08be6e5cd8abac
Ismailn13 <90825719+Ismailn13@users.noreply.github.com>
2021-11-01 13:54:14 +0400

    initial commit

--- game.js
+++ game.js
@@ -152,5 +152,23 @@ class Bee {
       i++;
       }
       }
+      function moveBees() {
+        //get speed input field value
+         let speed = document.getElementById("speedBees").value;
+        //move each bee to a random location
+        for (let i = 0; i < bees.length; i++) {
+        let dx = getRandomInt(2 * speed) - speed;
+        let dy = getRandomInt(2 * speed) - speed;
+        bees[i].move(dx, dy);
+        }
+        }
+        function updateBees() { // update loop for game
+          //move the bees randomly
+          moveBees();
+          //use a fixed update period
+          let period = 10;//modify this to control refresh period
+          //update the timer for the next move
+          updateTimer = setTimeout('updateBees()', period);
+          }
   }
 }
